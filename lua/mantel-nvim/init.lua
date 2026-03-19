24b01b048e3160098b01a7d67c82913040721d94
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2026-03-19 00:09:49 -0300

    docs: huge docs update and restructure

--- lua/mantel-nvim/init.lua
+++ lua/mantel-nvim/init.lua
@@ -8,7 +8,7 @@ local tabline = require("mantel-nvim.ui.tabline")
 local M = {}
 
 M.patch = "0"
-M.minor = "26"
+M.minor = "27"
 M.major = "0"
 
 M.version = M.major .. "." .. M.minor .. "." .. M.patch
